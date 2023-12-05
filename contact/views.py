0082fcf44b305eaf2bfa01ecd7467cfa4d455210
Lucas Menezes <lu289997@gmail.com>
2023-12-04 22:12:50 -0300

    create template

--- contact/views.py
+++ contact/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 
-# Create your views here.
-def index(request):
-    return render(
-        request,
-        'contact/index.html',
-    )
+# # Create your views here.
+# def index(request):
+#     return render(
+#         request,
+#         'contact/index.html',
+#     )
