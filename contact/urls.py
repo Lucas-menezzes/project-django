8ea24e95b204c89dac3213066b0fb665edd148f2
Lucas Menezes <lu289997@gmail.com>
2023-12-05 08:01:42 -0300

    create pagination

--- contact/urls.py
+++ contact/urls.py
@@ -5,6 +5,8 @@ app_name = 'contact'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:contact_id>/', views.contact, name='contact'),
+    # path('<int:contact_id>/', views.contact, name='contact'),
+    path('contact/<int:contact_id>/', views.contact, name='contact'),
     path('search/', views.search, name='search'),
+    
 ]
--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import get_object_or_404, redirect, render
 from contact.models import Contact
 from django.db.models import Q
+from django.core.paginator import Paginator
 
 # Create your views here.
 def index(request):
     contacts = Contact.objects\
         .filter(show=True)\
         .order_by('-id')
+    
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
     context={
+        'page_obj': page_obj,
         'contacts': contacts,
     }
     return render(
