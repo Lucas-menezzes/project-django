05bf24243c47879a476a8551f3b4cad97e7b75e0
Lucas Menezes <lu289997@gmail.com>
2023-12-31 21:14:06 -0300

    change update user

--- contact/forms.py
+++ contact/forms.py
@@ -5,6 +5,7 @@ from . import models
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import password_validation
 
 
 class ContactForm(forms.ModelForm):
@@ -70,9 +71,11 @@ class RegisterForm(UserCreationForm):
 
     def clean_email(self):
         email = self.cleaned_data.get('email')
+        current_email = self.instance.email
 
-        if User.objects.filter(email=email).exists():
-            raise forms.ValidationError('Email ja cadastrado')
+        if current_email != email:
+            if User.objects.filter(email=email).exists():
+                raise forms.ValidationError('Email ja cadastrado')
         
         return email
     
@@ -80,3 +83,43 @@ class LoginForm(AuthenticationForm):
     class Meta:
         model = AuthenticationForm
         fields = ('username', 'password')
+
+class RegisterUpdateForm(forms.ModelForm):
+    first_name = forms.CharField(
+        min_length=2,
+        max_length=30,
+        required=True,
+        help_text='Required'
+    )
+    
+    last_name = forms.CharField(
+        min_length=2,
+        max_length=30,
+        required=True,
+        help_text='Required'
+    )
+    password1 = forms.CharField(
+        label='Password 1',
+        strip=False,
+        widget=forms.PasswordInput(attrs={'autocomplete': 'new-password'}),
+        help_text=password_validation.password_validators_help_text_html(),
+        required=False,
+    )
+    password2 = forms.CharField(
+        label='Password 2',
+        strip=False,
+        widget=forms.PasswordInput(attrs={'autocomplete': 'new-password'}),
+        help_text=password_validation.password_validators_help_text_html(),
+        required=False,
+    )
+
+    class Meta:
+        model = User
+        fields = (
+            'first_name',
+            'last_name',
+            'email',
+            'username',
+            'password1',
+            'password2',
+        )
--- contact/views/user_forms.py
+++ contact/views/user_forms.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.forms import AuthenticationForm
 from django.shortcuts import render, redirect
-from contact.forms import RegisterForm
+from contact.forms import RegisterForm, RegisterUpdateForm
 from django.contrib import messages
 from django.contrib import auth
 from django.contrib.auth import logout 
@@ -29,7 +29,6 @@ def register(request):
     )
 
 def authenticator(request):
-
     form = AuthenticationForm(request)
     
     if request.method == 'POST':
@@ -56,3 +55,31 @@ def authenticator(request):
 def logout_view(request):
     logout(request)
     return redirect('contact:authenticator')
+
+def user_update(request):
+    form = RegisterUpdateForm(instance=request.user)
+
+    if request.method != 'POST':
+        return render(
+            request, 'contact/register.html',
+            {
+                'form': form
+            }
+        )
+    form = RegisterUpdateForm(data=request.POST ,instance=request.user)
+
+    if not form.is_valid():
+        return render(
+            request, 'contact/register.html',
+            {
+                'form': form
+            }
+        )
+    form.save()
+    messages.success(request, 'perfil atualizado com sucesso')
+    return render(           
+        request, 'contact/register.html',
+            {
+                'form': form
+            }
+        )
